15657cc4472e2d03bc67390c195130886ec60d78
Nitzan-Beck <s1494@motzkine.ort.org.il>
2020-10-19 14:23:56 +0300

    19.10.2020

--- app/src/main/java/com/example/nb_2nd_assignment/MainActivity.java
+++ app/src/main/java/com/example/nb_2nd_assignment/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.nb_2nd_assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int x=0;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.txtPoint);
+    }
+
+    public void btnPlus(View view)
+    {
+        x++;
+        String st=getString(R.string.textOnTextBox);
+        tv.setText(st+" "+x);
     }
 }
